dcd56ad86d1f66db0577de0432d86ed710ed2982
rammpk <rammpk@gmail.com>
2025-08-19 15:22:51 +0000

    validate todo

--- src/core/todo/factories/make-validated-todo.spec.ts
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -1,24 +1,76 @@
 import * as sanitizeStrMod from "@/utils/sanitize-str";
-import { makeValidatedTodo } from "./make-validated-todo";
+import * as validationTodoDescriptionMod from "@/core/todo/schemas/validate-todo-description";
+import * as makeNewTodoMod from "@/core/todo/factories/make-new-todo";
+import { InvalidTodo, makeValidatedTodo, ValidTodo } from "./make-validated-todo";
+import { Todo } from "../schemas/todo.contract";
+import { randomUUID } from "crypto";
 
-const description = "abcdes"
-const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, "sanitizeStr")
+function makeMocks(description: string) {
+    const errors = ["Invalid todo description"]
+
+    const todo: Todo = {
+        id: randomUUID(),
+        description: description,
+        createdAt: new Date().toISOString()
+    }
+    const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, "sanitizeStr").mockReturnValue(description)
+    const validateTodoDescriptionSpy = vi.spyOn(validationTodoDescriptionMod, "validateTodoDescription").mockReturnValue({
+        success: true,
+        errors: []
+    })
+    const makeNewTodoSpy = vi.spyOn(makeNewTodoMod, "makeNewTodo").mockReturnValue(todo)
+
+    return {
+        errors,
+        todo,
+        description,
+        sanitizeStrSpy,
+        validateTodoDescriptionSpy,
+        makeNewTodoSpy
+    }
+}
 
 describe("makeValidatedTodo (unit)", () => {
     it("should call sanitizeStr with a correct vlaue", () => {
-        sanitizeStrSpy.mockReturnValue(description)
+        const { sanitizeStrSpy, description } = makeMocks("abcded")
 
         makeValidatedTodo(description)
 
-        expect(sanitizeStrSpy).toHaveBeenCalledTimes(1)
-        expect(sanitizeStrSpy).toHaveBeenCalledWith(description)
+        expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description)
     })
 
     it("should call validateTodoDescription with sanitized return value", () => {
+        const { sanitizeStrSpy, validateTodoDescriptionSpy } = makeMocks("abcded")
+        
+        const sanitizedReturn = "novo retorno"
+        sanitizeStrSpy.mockReturnValue(sanitizedReturn)
 
+        makeValidatedTodo(sanitizedReturn)
+
+        expect(validateTodoDescriptionSpy).toHaveBeenCalledExactlyOnceWith(sanitizedReturn)
     })
 
-    it("should call makeNewTodo if validateTodoDescription returned success", () => { })
+    it("should call makeNewTodo if validateTodoDescription returned success", () => {
+        const { description, todo } = makeMocks("Novo todo")
 
-    it("should retornar validateDescription.error if validation failed", () => { })
+        const result = makeValidatedTodo(description) as ValidTodo
+
+        expect(result.success).toBe(true)
+        expect(result.data.id).toBe(todo.id)
+        expect(result.data.description).toBe(todo.description)
+        expect(result.data.createdAt).toStrictEqual(todo.createdAt)
+    })
+
+    it("should retornar validateDescription.error if validation failed", () => {
+        const { description, validateTodoDescriptionSpy, errors } = makeMocks(123 as any)
+        validateTodoDescriptionSpy.mockReturnValue({
+            success: false,
+            errors,
+        })
+
+        const result = makeValidatedTodo(description) as InvalidTodo
+
+        expect(result.success).toBe(false)
+        expect(result.errors).toStrictEqual(errors)
+    })
 })
